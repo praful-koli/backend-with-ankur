308292f18f786e0fd9db246bb713d779a8cfc6a2
praful-koli <prafulkoli852@gmail.com>
2026-05-27 11:02:49 +0530

    database config

--- project/notes/1stAttempt/server.js
+++ project/notes/1stAttempt/server.js
@@ -1,5 +1,7 @@
 import app from "./src/app.js";
+import connectDB  from "./src/config/db.js";
 
+await connectDB()
 app.listen(3000, ()=> {
     console.log(`Server is Runing on port 3000`)
 })
---
+++ project/notes/1stAttempt/src/config/db.js
@@ -0,0 +1,14 @@
+// Database configure
+import mongoose from 'mongoose'
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL)
+        console.log('Database connected successfully')
+    } catch (error) {
+        console.log('Database connection failed ', error)
+    }
+}
+
+
+export default connectDB

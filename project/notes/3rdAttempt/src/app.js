38f95d89a5ac3efb69a818a81b00e38faaa04618
praful-koli <prafulkoli852@gmail.com>
2026-05-27 16:51:11 +0530

    feat(api): get all notes api implement

--- project/notes/3rdAttempt/src/app.js
+++ project/notes/3rdAttempt/src/app.js
@@ -52,4 +52,37 @@ app.post('/api/notes' , async (req , res) => {
     }
 })
 
+/**
+ * @route GET /api/notes
+ * @description Get all Notes
+ * @access Public
+ */
+
+app.get('/api/notes', async (req, res) => {
+    try {
+       
+      const notes = await noteModel.find()
+
+      if (notes.length == 0) {
+         return res.status(404).json({
+            error : "Notes not Found"
+         })
+      } 
+
+      res.status(200).json({
+         message : "fetch all notes sucessfuly",
+         notes
+      })
+        
+    } catch (error) {
+        return res.status(500).json({
+            message : "internal server error",
+            error : error.message
+        })
+    }
+})
+
+
+
+
 export default app
